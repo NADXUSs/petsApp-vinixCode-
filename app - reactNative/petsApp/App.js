7dd5b7aeeb372a32e335e943277a23a6b73b6714
Leider Acevedo <programador9@solucionesstar.com>
2022-05-27 00:09:06 -0500

    pets app v0.1

--- app - reactNative/petsApp/App.js
+++ app - reactNative/petsApp/App.js
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={Home} options={{ headerShown: false }} />
+        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
         <Stack.Screen name="Register" component={Register} options={{ headerShown: false }} />
         <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
       </Stack.Navigator>
